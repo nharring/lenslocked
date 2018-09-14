c37423e485cc21c02a08fff27935c4952e4d302a
Nicholas Harring <nharring@gmail.com>
2018-09-13 19:28:13 -0700

    stuff

--- main.go
+++ main.go
@@ -1,38 +1,38 @@
 package main
 
 import (
-  "fmt"
-  "net/http"
+	"fmt"
+	"net/http"
 
-  "github.com/gorilla/mux"
+	"github.com/julienschmidt/httprouter"
 )
 
-func home(w http.ResponseWriter, r *http.Request) {
-  w.Header().Set("Content-Type", "text/html")
-  fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
+func home(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
 }
-func contact(w http.ResponseWriter, r *http.Request) {
-  w.Header().Set("Content-Type", "text/html")
-  fmt.Fprint(w, "To get in touch, please send an email "+
-    "to <a href=\"mailto:fake@address.com\">"+
-    "fake@address.com</a>")
+func contact(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "To get in touch, please send an email "+
+		"to <a href=\"mailto:fake@address.com\">"+
+		"fake@address.com</a>")
 }
-func faq(w http.ResponseWriter, r *http.Request) {
-  w.Header().Set("Content-Type", "text/html")
-  fmt.Fprint(w, "<h1>Frequently Asked Questions:</h1></p> "+
-    "1. Yes? No</p>")
+func faq(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "<h1>Frequently Asked Questions:</h1></p> "+
+		"1. Yes? No</p>")
 }
 func notfound(w http.ResponseWriter, r *http.Request) {
-    w.WriteHeader(http.StatusNotFound)
-    fmt.Fprint(w, "<h1>Unable to find the page you requested.</h1>")
+	w.WriteHeader(http.StatusNotFound)
+	fmt.Fprint(w, "<h1>Unable to find the page you requested.</h1>")
 }
 
 func main() {
-  r := mux.NewRouter()
-  r.HandleFunc("/", home)
-  r.HandleFunc("/contact", contact)
-  r.HandleFunc("/faq", faq)
-  var h http.Handler = http.HandlerFunc(notfound)
-  r.NotFoundHandler = h
-  http.ListenAndServe(":3000", r)
+	router := httprouter.New()
+	router.GET("/", home)
+	router.GET("/contact", contact)
+	router.GET("/faq", faq)
+	var h http.Handler = http.HandlerFunc(notfound)
+	router.NotFound = h
+	http.ListenAndServe(":3000", router)
 }
