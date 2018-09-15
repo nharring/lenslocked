c916bf4474bafb18156acbfbc89fead97fc9198b
Nicholas Harring <nharring@gmail.com>
2018-09-15 15:38:17 -0700

    Users controller v0

--- main.go
+++ main.go
@@ -5,13 +5,13 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"lenslocked.com/controllers"
 	"lenslocked.com/views"
 )
 
 var homeView *views.View
 var contactView *views.View
 var faqView *views.View
-var signupView *views.View
 
 func must(err error) {
 	if err != nil {
@@ -35,21 +35,18 @@ func notfound(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusNotFound)
 	fmt.Fprint(w, "<h1>Unable to find the page you requested.</h1>")
 }
-func signup(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	must(signupView.Render(w, nil))
-}
 
 func main() {
 	homeView = views.NewNamedView("bootstrap", "home")
 	contactView = views.NewNamedView("bootstrap", "contact")
 	faqView = views.NewNamedView("bootstrap", "faq")
-	signupView = views.NewNamedView("bootstrap", "signup")
+	usersC := controllers.NewUsers()
 	r := mux.NewRouter()
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
 	r.HandleFunc("/faq", faq)
-	r.HandleFunc("/signup", signup)
+	r.HandleFunc("/signup", usersC.New).Methods("GET")
+	r.HandleFunc("/signup", usersC.Create).Methods("POST")
 	r.NotFoundHandler = http.HandlerFunc(notfound)
 	http.ListenAndServe(":3000", r)
 }
